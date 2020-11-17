990e4301e7c1a2b27aacc2767f60e8cb92d93dad
fvitte <74621672+fvitte@users.noreply.github.com>
2020-11-18 01:36:22 +0500

    Выполнял задание по изменению файлов в репозитории

--- my_script.js
+++
@@ -1,7 +0,0 @@
-function moveButton1(){
-	document.getElementById('img1').innerHTML = "<img src='picture2.jpg' width=30 high=30 alt='Подмигивающий смайл' style='vertical-align: middle'> Кнопка с рисунком ;-)</button>";
-}
-function moveButton2(){
-	document.getElementById('img2').innerHTML = "<img src="picture1.jpg" width=30 high=30 alt="Улыбающийся смайл" style="vertical-align: middle"> Кнопка с рисунком :)</button>";
-}
-//document.getElementById('dd').innerHTML = "<h1>Сегодняшняя дата: " + d +"</h1>";
